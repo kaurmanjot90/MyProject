6e2a74a352ff7644cc0be27da581536eba1479e7
Manjot Kaur <manjotkaur@Manjots-MacBook-Air.local>
2023-08-12 19:43:07 -0400

    v3

--- src/main/java/com/qa/appn/pages/ProductInfoPage.java
+++ src/main/java/com/qa/appn/pages/ProductInfoPage.java
@@ -1,15 +1,26 @@
 package com.qa.appn.pages;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.qa.appn.utils.ElementUtil;
+import com.qa.appn.utils.TimeUtil;
 
 public class ProductInfoPage {
 	
 	private WebDriver driver;
 	private ElementUtil eleUtil;
 	private By productHeader = By.cssSelector("div#content h1");
+	private By productImages = By.cssSelector("a.thumbnail");
+	private By productMetadata = By.xpath("");
+	private By pricingData = By.xpath("");
+	
+	private Map<String, String> productMap;
 	
 	public ProductInfoPage(WebDriver driver) {
 		this.driver = driver;
@@ -18,5 +29,43 @@ public class ProductInfoPage {
 
 	public String getProductHeader() {
 		return eleUtil.doGetElementText(productHeader);
+	}
+	
+	public int getProductImagesCount() {
+		int ImagesCount = eleUtil.waitForElementsVisible(productImages, TimeUtil.DEFAULT_TIME_OUT).size();
+		System.out.println("Total Image Count on product info page = "+ImagesCount);
+	    return ImagesCount;
+	}
+	
+	
+	public Map<String, String> getProductInformation() { //5.
+		productMap = new HashMap<String, String>();
+		getProductMetadata();
+		getProductPricingdata();
+		System.out.println(productMap);
+		return productMap;
+		
+	}
+
+	private void getProductMetadata() { 
+		List<WebElement> metaDataList = eleUtil.getElements(productMetadata);
+		System.out.println("product metadata count -->"+ metaDataList.size());
+		
+		for(WebElement e : metaDataList) {
+			String meta = e.getText();
+			String metaData[] = meta.split(":");
+			String metaKey = metaData[0].trim();
+			String metaValue = metaData[1].trim();
+			productMap.put(metaKey, metaValue);
+		}
+	}
+		private void getProductPricingdata() {
+			List<WebElement> metaPriceList = eleUtil.getElements(pricingData);
+			System.out.println("product rice count -->"+ metaPriceList.size());
+			String price = metaPriceList.get(0).getText().trim();
+			String ExTaxPrice = metaPriceList.get(0).getText().trim();
+			
+			productMap.put("actprice", price);
+			productMap.put("actExTaxPrice", ExTaxPrice);
 	}
 }
--- src/test/java/com/qa/appn/tests/ProductInfoPageTest.java
+++ src/test/java/com/qa/appn/tests/ProductInfoPageTest.java
@@ -1,5 +1,7 @@
 package com.qa.appn.tests;
 
+import java.util.Map;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -30,5 +32,35 @@ public class ProductInfoPageTest extends BaseTest {
 		productInfoPage = resultsPage.selectProduct(mainProductName);
 		String actHeader = productInfoPage.getProductHeader();
 		Assert.assertEquals(actHeader, mainProductName);
+	}
+	
+	@DataProvider
+	public Object[][] getProductImagesTestData() {
+		return new Object[][] { //5.
+			{"Macbook","MacBook Pro", 4},
+			{"Macbook","MacBook Air", 4},
+			{"iMac","iMac", 3},
+			{"Apple","Apple Cinema 30\"", 6}
+		};	
+	}
+	
+	@Test(dataProvider="getProductImagesTestData")
+	public void productImagesTest(String searchKey, String mainProductName, int imageCount) {
+		resultsPage = accPage.performSearch(searchKey);//4. zig-zag pattern
+		productInfoPage = resultsPage.selectProduct(mainProductName);
+		int actImageCount = productInfoPage.getProductImagesCount();
+		Assert.assertEquals(actImageCount, imageCount);
+	}
+	
+	@Test
+	public void productMetadataTest() {
+		resultsPage = accPage.performSearch("Macbook");
+		productInfoPage = resultsPage.selectProduct("MacBook Pro");
+		Map<String, String> actProductInfoMap = productInfoPage.getProductInformation();
+		
+		softAssert.assertEquals(actProductInfoMap.get("Brand"), "Apple");
+		softAssert.assertEquals(actProductInfoMap.get("Availablitiy"), "In Stock");
+		softAssert.assertEquals(actProductInfoMap.get("actualprice"), "$2000.00");
+		softAssert.assertAll();//5. which assertion got failed - gives all info.
 	}
 }
